ea89b73bdeaae90271898b60b142ed82b9c952d3
Daniel Kambich <daniel.kambich@gmail.com>
2020-12-26 11:50:25 -0600

    Add cards for trending

--- src/pages/Trending.tsx
+++ src/pages/Trending.tsx
@@ -1,105 +1,166 @@
 import React, { useEffect, useState } from "react";
-import { Card, Container, Header, Image, Loader } from "semantic-ui-react";
+import {
+  Card,
+  Container,
+  Grid,
+  Header,
+  Image,
+  Loader,
+} from "semantic-ui-react";
+import { ResponsiveLine } from "@nivo/line";
 import Navbar from "../navigation/Navbar";
 
-const coins = [
+const data = [
   {
-    item: {
-      id: "1inch",
-      name: "1inch",
-      symbol: "1INCH",
-      market_cap_rank: 97,
-      thumb:
-        "https://assets.coingecko.com/coins/images/13469/thumb/1inch-token.png?1608803028",
-      large:
-        "https://assets.coingecko.com/coins/images/13469/large/1inch-token.png?1608803028",
-      score: 0,
-    },
-  },
-  {
-    item: {
-      id: "dynamic-set-dollar",
-      name: "Dynamic Set Dollar",
-      symbol: "DSD",
-      market_cap_rank: 104,
-      thumb:
-        "https://assets.coingecko.com/coins/images/13249/thumb/DSD.jpg?1606713628",
-      large:
-        "https://assets.coingecko.com/coins/images/13249/large/DSD.jpg?1606713628",
-      score: 1,
-    },
-  },
-  {
-    item: {
-      id: "marlin",
-      name: "Marlin",
-      symbol: "POND",
-      market_cap_rank: 319,
-      thumb:
-        "https://assets.coingecko.com/coins/images/8903/thumb/Marlin.png?1608584519",
-      large:
-        "https://assets.coingecko.com/coins/images/8903/large/Marlin.png?1608584519",
-      score: 2,
-    },
-  },
-  {
-    item: {
-      id: "the-graph",
-      name: "The Graph",
-      symbol: "GRT",
-      market_cap_rank: 54,
-      thumb:
-        "https://assets.coingecko.com/coins/images/13397/thumb/Graph_Token.png?1608145566",
-      large:
-        "https://assets.coingecko.com/coins/images/13397/large/Graph_Token.png?1608145566",
-      score: 3,
-    },
-  },
-  {
-    item: {
-      id: "seigniorage-shares",
-      name: "Seigniorage Shares",
-      symbol: "SHARE",
-      market_cap_rank: 412,
-      thumb:
-        "https://assets.coingecko.com/coins/images/12306/thumb/logo_%281%29.png?1607658707",
-      large:
-        "https://assets.coingecko.com/coins/images/12306/large/logo_%281%29.png?1607658707",
-      score: 4,
-    },
-  },
-  {
-    item: {
-      id: "ripple",
-      name: "XRP",
-      symbol: "XRP",
-      market_cap_rank: 4,
-      thumb:
-        "https://assets.coingecko.com/coins/images/44/thumb/xrp-symbol-white-128.png?1605778731",
-      large:
-        "https://assets.coingecko.com/coins/images/44/large/xrp-symbol-white-128.png?1605778731",
-      score: 5,
-    },
-  },
-  {
-    item: {
-      id: "uniswap",
-      name: "Uniswap",
-      symbol: "UNI",
-      market_cap_rank: 40,
-      thumb:
-        "https://assets.coingecko.com/coins/images/12504/thumb/uniswap-uni.png?1600306604",
-      large:
-        "https://assets.coingecko.com/coins/images/12504/large/uniswap-uni.png?1600306604",
-      score: 6,
-    },
+    id: "japan",
+    color: "hsl(240, 70%, 50%)",
+    data: [
+      {
+        x: "plane",
+        y: 0,
+      },
+      {
+        x: "helicopter",
+        y: 51,
+      },
+      {
+        x: "boat",
+        y: 46,
+      },
+      {
+        x: "train",
+        y: 15,
+      },
+      {
+        x: "subway",
+        y: 208,
+      },
+      {
+        x: "bus",
+        y: 72,
+      },
+      {
+        x: "car",
+        y: 138,
+      },
+      {
+        x: "moto",
+        y: 200,
+      },
+      {
+        x: "bicycle",
+        y: 152,
+      },
+      {
+        x: "horse",
+        y: 148,
+      },
+      {
+        x: "skateboard",
+        y: 228,
+      },
+      {
+        x: "others",
+        y: 104,
+      },
+    ],
   },
 ];
 
+const Graph = () => (
+  <div style={{ height: 200, width: "auto" }}>
+    <ResponsiveLine
+      data={data}
+      margin={{ top: 50, right: 80, bottom: 50, left: 60 }}
+      xScale={{ type: "point" }}
+      yScale={{
+        type: "linear",
+        min: 0,
+        max: "auto",
+        stacked: true,
+        reverse: false,
+      }}
+      yFormat=" >-.2f"
+      axisTop={null}
+      axisRight={null}
+      axisBottom={{
+        orient: "bottom",
+        tickSize: 5,
+        tickPadding: 5,
+        tickRotation: 0,
+        legend: "transportation",
+        legendOffset: 36,
+        legendPosition: "middle",
+      }}
+      axisLeft={{
+        orient: "left",
+        tickSize: 5,
+        tickPadding: 5,
+        tickRotation: 0,
+        legend: "count",
+        legendOffset: -40,
+        legendPosition: "middle",
+      }}
+      pointSize={10}
+      pointColor={{ theme: "background" }}
+      pointBorderWidth={2}
+      pointBorderColor={{ from: "serieColor" }}
+      pointLabelYOffset={-12}
+      useMesh={true}
+      legends={[
+        {
+          anchor: "bottom-right",
+          direction: "column",
+          justify: false,
+          translateX: 100,
+          translateY: 0,
+          itemsSpacing: 0,
+          itemDirection: "left-to-right",
+          itemWidth: 80,
+          itemHeight: 20,
+          itemOpacity: 0.75,
+          symbolSize: 12,
+          symbolShape: "circle",
+          symbolBorderColor: "rgba(0, 0, 0, .5)",
+          effects: [
+            {
+              on: "hover",
+              style: {
+                itemBackground: "rgba(0, 0, 0, .03)",
+                itemOpacity: 1,
+              },
+            },
+          ],
+        },
+      ]}
+    />
+  </div>
+);
+interface TrendingCoin {
+  id: string;
+  name: string;
+  symbol: string;
+  market_cap_rank: number;
+  thumb: string;
+  large: string;
+  score: number;
+}
+
+async function getTrendingData(): Promise<TrendingCoin[]> {
+  let resp = await fetch("https://api.coingecko.com/api/v3/search/trending");
+  let respJSON = await resp.json();
+  return respJSON.coins.map((coin: any) => coin.item);
+}
+
 function Trending() {
   const [loading, setLoading] = useState(true);
+  const [trending, setTrending] = useState<TrendingCoin[]>([]);
+
   useEffect(() => {
-    setTimeout(() => setLoading(false), 1500);
+    getTrendingData().then((coins) => {
+      setLoading(false);
+      setTrending(coins);
+    });
   }, []);
 
   return (
@@ -111,27 +172,32 @@ function Trending() {
             <Loader active={loading} inline="centered" />
           </div>
         )}
-        {!loading &&
-          coins.map((coin) => (
-            <Card
-              style={{
-                width: "100%",
-                margin: "1.5em",
-                padding: "2.5em",
-                display: "flex",
-              }}
+        <Grid container centered columns={2} style={{ margin: "1.5em" }}>
+          {trending.map(({ id, symbol, market_cap_rank, name, large }) => (
+            <Grid.Column
+              key={id}
+              style={{ display: "flex", justifyContent: "center" }}
             >
-              <div style={{ display: "flex", alignItems: "center" }}>
-                <Image src={coin.item.large} size="mini" />
-                <Header as="h2" style={{ margin: 0, marginLeft: "1em" }}>
-                  {coin.item.name} ({coin.item.symbol})
-                  <Header.Subheader>
-                    Current market cap rank {coin.item.market_cap_rank}
-                  </Header.Subheader>
-                </Header>
-              </div>
-            </Card>
+              <Card>
+                <Graph />
+                <Card.Content>
+                  <Card.Header>
+                    <div style={{ display: "flex", alignItems: "center" }}>
+                      <Image src={large} size="mini" />
+                      <Header as="h5" style={{ margin: 0, marginLeft: "1em" }}>
+                        {name} ({symbol})
+                        <Header.Subheader>
+                          Current market cap rank {market_cap_rank}
+                        </Header.Subheader>
+                      </Header>
+                    </div>
+                  </Card.Header>
+                </Card.Content>
+              </Card>
+            </Grid.Column>
           ))}
+        </Grid>
+        <div style={{ height: 200 }}></div>
       </Container>
     </div>
   );
