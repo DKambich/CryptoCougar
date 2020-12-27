b86a72ac22a2b91976c815b1fddd818a2a23159e
Daniel Kambich <daniel.kambich@gmail.com>
2020-12-26 23:39:48 -0600

    Fix formatting and cleanup

--- src/pages/Trending.tsx
+++ src/pages/Trending.tsx
@@ -6,6 +6,7 @@ import {
   Header,
   Image as SemanticImage,
   Loader,
+  Message,
 } from "semantic-ui-react";
 import { Datum, ResponsiveLine, Serie } from "@nivo/line";
 import moment from "moment";
@@ -31,19 +32,25 @@ const Graph = ({
   return (
     <div style={{ height: 300, width: "auto" }}>
       <ResponsiveLine
-        // tooltip={({ point }) => {
-        //   console.log(point);
-        //   return <Card>Date: {point.data.xFormatted}</Card>;
-        // }}
         data={[series]}
         colors={lineColor}
-        curve="natural"
+        curve="cardinal"
+        useMesh
+        tooltip={({ point }) => (
+          <Message>
+            {point.data.yFormatted} on {point.data.xFormatted}
+          </Message>
+        )}
+        pointSize={8}
         margin={{ top: 50, right: 80, bottom: 50, left: 80 }}
-        xScale={{ type: "point" }}
-        yScale={{
-          type: "linear",
+        axisLeft={{
+          tickSize: 10,
+          tickPadding: 5,
+          legend: "Price",
+          legendOffset: -55,
+          legendPosition: "middle",
         }}
-        yFormat=" >-$.2r"
+        yFormat=" >-$.3r"
         axisBottom={{
           tickSize: 10,
           tickPadding: 5,
@@ -52,16 +59,6 @@ const Graph = ({
           legendPosition: "middle",
           format: (value) => moment(value).format("MM/DD/YY"),
         }}
-        axisLeft={{
-          tickSize: 10,
-          tickPadding: 5,
-          legend: "Price",
-          legendOffset: -55,
-          legendPosition: "middle",
-        }}
-        pointSize={8}
-        pointBorderColor={{ from: "serieColor" }}
-        useMesh={true}
       />
     </div>
   );
@@ -90,8 +87,7 @@ interface TrendingData extends TrendingCoin {
 
 const convertToDatum = (data: number[][]): Datum[] => {
   return data.map((item) => {
-    console.log(moment(item[0]).format("MM/DD/YY"));
-    return { x: moment(item[0]).format("MM/DD/YY"), y: item[1] };
+    return { x: moment(item[0]).format("h:mm A MM/DD/YYYY"), y: item[1] };
   });
 };
 
@@ -144,13 +140,7 @@ const renderCard = ({
   color,
   historicData: { prices },
 }: TrendingData) => (
-  <Grid.Column
-    key={id}
-    mobile={16}
-    tablet={16}
-    computer={8}
-    style={{ display: "flex", justifyContent: "center" }}
-  >
+  <Grid.Column key={id} mobile={16} tablet={16} computer={8}>
     <Card style={{ width: "100%" }}>
       <Graph id={name} data={convertToDatum(prices)} lineColor={color} />
       <Card.Content>
@@ -181,7 +171,7 @@ function Trending() {
   }, []);
 
   return (
-    <div style={{ display: "flex", flexDirection: "column", height: "100vh" }}>
+    <>
       <Navbar />
       <Container>
         {loading && (
@@ -192,9 +182,8 @@ function Trending() {
         <Grid container centered columns={2} style={{ margin: "1.5em" }}>
           {trending.map(renderCard)}
         </Grid>
-        <div style={{ height: 200 }}></div>
       </Container>
-    </div>
+    </>
   );
 }
 
